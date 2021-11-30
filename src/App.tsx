83fdcf845e956414e508ccd75a866b3e096d8aaf
Konstantin Lobanov <klobanov@pixsystem.com>
2021-11-30 23:10:12 +0200

    init: add entrypoints file and App component

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import { Exchange } from 'features/balances/Exchange';
+
+export default function App() {
+    return (
+        <div>
+            <Exchange/>
+        </div>
+    );
+}
