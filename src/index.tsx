93169dcb91c81cbadb1e9ebaba6e8762ba9724e8
Konstantin Lobanov <klobanov@pixsystem.com>
2021-12-15 01:04:20 +0200

    update entrypoint

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
+import store from 'store';
 
+import GlobalStyle from 'components/GlobalStyle';
 import App from './App';
 
 ReactDOM.render(
     <React.StrictMode>
-        <App />
+        <Provider store={store}>
+            <GlobalStyle />
+            <App />
+        </Provider>
     </React.StrictMode>,
-    document.getElementById('root')
+    document.getElementById('root'),
 );
-
-console.log('aaa');
